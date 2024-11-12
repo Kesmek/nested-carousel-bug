0db509caf207dd4aa0f59de40b8dda8116240543
Justin <justin.scopelleti@gmail.com>
2024-11-11 23:41:53 -0500

    bug: Carousel nested in a modal breaks carousel bindings

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,50 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal, $ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import { Carousel, Modal } from "@qwik-ui/headless";
+
+const colors = ["red", "blue", "green", "yellow", "purple", "orange", "teal"];
 
 export default component$(() => {
+  const selectedIndex = useSignal(0);
+
+  const incrementIndex$ = $(() => {
+    selectedIndex.value++;
+  });
+
+  const decrementIndex$ = $(() => {
+    if (selectedIndex.value > 0) {
+      selectedIndex.value--;
+    }
+  });
+
   return (
     <>
-      <h1>Hi 👋</h1>
-      <div>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </div>
+      <Modal.Root>
+        <Modal.Trigger>Show Carousel</Modal.Trigger>
+        <div>Carousel Selected index: {selectedIndex.value}</div>
+        <div>
+          <button onClick$={decrementIndex$}>-</button>
+          <button onClick$={incrementIndex$}>+</button>
+        </div>
+        <Modal.Panel>
+          <Carousel.Root
+            bind:selectedIndex={selectedIndex}
+            startIndex={selectedIndex.value}
+          >
+            <div>
+              <Carousel.Previous>Previous</Carousel.Previous>
+              <Carousel.Next>Next</Carousel.Next>
+            </div>
+            <Carousel.Scroller>
+              {colors.map((color, index) => (
+                <Carousel.Slide
+                  key={color}
+                >{`Color: ${color}, Index: ${index}`}</Carousel.Slide>
+              ))}
+            </Carousel.Scroller>
+          </Carousel.Root>
+        </Modal.Panel>
+      </Modal.Root>
     </>
   );
 });
